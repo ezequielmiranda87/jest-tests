06d2f89a045742ebf89e188747a5861948ddc3c8
ezex87 <ezequielmiranda87@gmail.com>
2019-01-30 18:45:17 +0100

    Init project

---
+++ math.js
@@ -0,0 +1,15 @@
+const sumar = (a, b) => {
+    return a + b;
+}
+
+const multiplicar = (a, b) =>{
+    return a * b;
+}
+
+const restar = (a, b) =>{
+    return a - b;
+}
+
+const dividir = (a, b) =>{
+    a / b;
+}
